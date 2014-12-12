bdfc8af0009257e63d14be390b4169054298f481
Utku Sonmez <utkusonmez@yahoo.com>
2014-12-12 17:32:37 +0200

    nested optional test.

--- src/test/java/optional/OptionalTest.java
+++ src/test/java/optional/OptionalTest.java
@@ -7,9 +7,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class OptionalTest {
 
@@ -41,6 +39,58 @@ public class OptionalTest {
 
         assertThat(optApple.map(Apple::getColor).get(), equalTo("red"));
         assertThat(optNullApple.map(Apple::getColor).isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void shouldTestNestedOptional() {
+        Optional<AppleBucket> appleBucketNull = Optional.ofNullable(null);
+        assertFalse(appleBucketNull.isPresent());
+
+        Optional<AppleBucket> appleBucketEmpty = Optional.of(new AppleBucket());
+        assertTrue(appleBucketEmpty.isPresent());
+        assertFalse(appleBucketEmpty.get().getApple1().isPresent());
+        assertFalse(appleBucketEmpty.get().getApple2().isPresent());
+
+        Optional<String> unknownWhenNotPresent = appleBucketEmpty.map(AppleBucket::getApple1).filter(Optional::isPresent).map(o -> o.get()).map(Apple::getColor);
+        assertThat(unknownWhenNotPresent.orElse("Unkown"), equalTo("Unkown"));
+
+        Optional<AppleBucket> appleBucketFull = Optional.of(new AppleBucket(new Apple("red", 100), new Apple("green", 150)));
+        assertTrue(appleBucketFull.isPresent());
+        assertTrue(appleBucketFull.get().getApple1().isPresent());
+        assertTrue(appleBucketFull.get().getApple2().isPresent());
+
+        Optional<String> apple1ColorOpt = appleBucketFull.map(AppleBucket::getApple1).map(o -> o.get()).map(Apple::getColor);
+        assertThat(apple1ColorOpt.get(), equalTo("red"));
+    }
+
+    private class AppleBucket {
+        private Optional<Apple> apple1 = Optional.empty();
+        private Optional<Apple> apple2 = Optional.empty();
+
+        public AppleBucket() {
+
+        }
+
+        public AppleBucket(Apple apple1, Apple apple2) {
+            this.apple1 = Optional.of(apple1);
+            this.apple2 = Optional.of(apple2);
+        }
+
+        public Optional<Apple> getApple1() {
+            return apple1;
+        }
+
+        public void setApple1(Optional<Apple> apple1) {
+            this.apple1 = apple1;
+        }
+
+        public Optional<Apple> getApple2() {
+            return apple2;
+        }
+
+        public void setApple2(Optional<Apple> apple2) {
+            this.apple2 = apple2;
+        }
     }
 
 }
